e792f168de43ce414e4469d336d596e802862532
osmanyb786 <bilalosmany1@gmail.com>
2024-09-12 15:20:13 +0530

    Update VMResoruces.tf

--- VMResoruces.tf
+++ VMResoruces.tf
@@ -1,11 +1,11 @@
 resource "azurerm_virtual_machine" "vm1" {
-  name                  = "testetstetestvm"
+  name                  = "bilalvm"
   resource_group_name   = "TerraformRG"
   location              = "East US"
   network_interface_ids = [azurerm_network_interface.nic1.id]
   vm_size               = "Standard_DS1_v2"
   storage_os_disk {
-    name = "vishnudisk1"
+    name = "bilaldisk1"
     caching = "ReadWrite"
     managed_disk_type = "Standard_LRS"
     create_option = "FromImage"
@@ -21,8 +21,8 @@ resource "azurerm_virtual_machine" "vm1" {
     version = "latest"
   }
   os_profile {
-    computer_name  = "trainingsystem"
-    admin_username = "trainingadmin"
+    computer_name  = "ibbisystem"
+    admin_username = "ibbiadmin"
     admin_password = "Accenture123$$"
   }
 
@@ -37,7 +37,7 @@ resource "azurerm_virtual_machine" "vm1" {
 }
 
 resource "azurerm_virtual_network" "vnet1" {
-  name                = "testetstetest"
+  name                = "ibbutest"
   address_space       = ["10.0.0.0/16"]
   location            = "East US"
   resource_group_name = "TerraformRG"
@@ -45,19 +45,19 @@ resource "azurerm_virtual_network" "vnet1" {
 
 
 resource "azurerm_subnet" "subnet1" {
-  name                 = "hrmante"
+  name                 = "ibbitest"
   resource_group_name  = "TerraformRG"
   virtual_network_name = azurerm_virtual_network.vnet1.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "nic1" {
-  name                = "vishnunic111"
+  name                = "ibbi111"
   location            = "East US"
   resource_group_name = "TerraformRG"
 
   ip_configuration {
-    name                          = "config111"
+    name                          = "ibbi111"
     subnet_id                     = azurerm_subnet.subnet1.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id =azurerm_public_ip.public1.id
@@ -65,7 +65,7 @@ resource "azurerm_network_interface" "nic1" {
 }
 
 resource "azurerm_public_ip" "public1" {
-  name = "testetstetest-tywteywgjh"
+  name = "ibbi-tywteywgjh"
   resource_group_name = "TerraformRG"
   location = "East US"
   allocation_method = "Static"
